993c9ec4957d6e5865086f829a65a4e0315bfba6
yara <yaraelbassaal157@gmail.com>
2023-02-17 15:30:04 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
