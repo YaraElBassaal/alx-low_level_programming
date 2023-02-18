76c99e007a7e6ca8efa9a203e14fad45cb75238d
yara <yaraelbassaal157@gmail.com>
2023-02-17 17:28:04 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print from 0 to 16 in hexadecimal
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int x = '0';
+
+while (x <= '9')
+{
+putchar(x);
+x++;
+}
+
+x = 'a';
+while (x <= 'f')
+{
+putchar(x);
+x++;
+}
+
+putchar('\n');
+return (0);
+}
