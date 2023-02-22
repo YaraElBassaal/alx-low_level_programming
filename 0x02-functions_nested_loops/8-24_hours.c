bc020845e5e0c1922719fa28692d62daaf0cb54a
yara <yaraelbassaal157@gmail.com>
2023-02-21 18:45:53 -0800

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+int m = 0;
+int h = 0;
+while (h <= 23)
+{
+while (m <= 59)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+if (h == 23 && m == 59)
+break;
+_putchar('\n');
+m++;
+}
+h++;
+m = 0;
+}
+_putchar('\n');
+}
