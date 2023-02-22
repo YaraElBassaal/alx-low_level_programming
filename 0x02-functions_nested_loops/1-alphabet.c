4695364476c58f5daf8876e99ac4e6942000e732
yara <yaraelbassaal157@gmail.com>
2023-02-21 18:33:49 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase
+ *
+ * Return: Nothing to return
+ */
+
+void print_alphabet(void)
+{
+int alpha = 'a';
+
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
