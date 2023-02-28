b4a75b2dd378b315bfcecc42059ff6aaf5462c00
yara <yaraelbassaal157@gmail.com>
2023-02-27 16:14:09 -0800

    66

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
 
 /**
  * puts2 - Write a function that prints every other character of a string
@@ -12,9 +12,12 @@ void puts2(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a += 2)
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		_putchar(s[a]);
+		if (a % 2 == 0)
+		{
+			_putchar(s[a]);
+		}
 	}
 	_putchar('\n');
 }
